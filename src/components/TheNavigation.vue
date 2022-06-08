1bba2e3a9c729da9672f3d85838e58d81eeed6a5
MattCrl <carlevaris@keensaas.fr>
2022-06-08 09:28:06 +0200

    Vue School travel app

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="nav">
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li v-for=""></li>
+    </ul>
+     
+      <router-link to="/brazil">Brazil</router-link>
+      <router-link to="/hawaii">Hawaii</router-link>
+      <router-link to="/jamaica">Jamaica</router-link>
+      <router-link to="/panama">Panama</router-link>
+    </div>
+</template>
+
+<style scoped>
+#nav {
+  display: flex;
+  align-items: center;
+  padding: 30px;
+
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 10px;
+}
+
+#nav a.vue-school-active-class {
+  color: #ab26ab;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -19,9 +19,40 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: () =>
+      import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue"),
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: () =>
+      import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue"),
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: () =>
+      import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue"),
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: () =>
+      import(/* webpackChunkName: "panama" */ "../views/Panama.vue"),
+  },
+  {
+    path: "/details/:id",
+    name: "DestinationDetails",
+    component: () =>
+      import(/* webpackChunkName: "details" */ "../views/DestinationDetails.vue")
+  }
 ];
 
 const router = new VueRouter({
+  linkExactActiveClass: "vue-school-active-class",
   routes,
 });
 
