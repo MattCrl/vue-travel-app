11b49801924fa7343e1064003d7838003ee82917
MattCrl <carlevaris@keensaas.fr>
2022-07-20 15:10:25 +0200

    Chapter 3 Lesson 2 - Route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <TheNavigation />
-    <router-view :key="$route.path" />
+    <Transition name="slide" mode="out-in">
+        <router-view :key="$route.path" />
+    </Transition>
   </div>
 </template>
 
@@ -13,9 +15,7 @@ export default {
     TheNavigation
   }
 }
-
 </script>
-
 
 <style>
 #app {
@@ -25,4 +25,59 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
+/* Slide transitions */
+.slide-enter-active, 
+.slide-leave-active {
+  transition: opacity 0.3s, transform 0.3s;
+}
+
+.slide-enter, 
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-5%);
+}
+
+/* MoveUp Transitions */
+.moveUp-enter-active {
+  animation: fadeIn 1s ease-in;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 0.5;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+
+.moveUp-leave-active {
+  animation: moveUp 0.3 ease-in;
+}
+
+@keyframes moveUp {
+  0% {
+    transform: translateY(0);
+  }
+
+  100% {
+    transform: translateY(-400px);
+  }
+}
+
+/* Fade transitions */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
 </style>
